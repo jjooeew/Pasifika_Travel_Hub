ffaf83dc308af213691e3cd1f550366c9ad18c81
GrahamBlackledge <gblackledge88@gmail.com>
2025-06-01 22:48:00 +1200

    Added background patterns

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,19 +1,28 @@
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import  NavBar from './components/NavBar/NavBar';
-import LandingPage from './pages/LandingPage';
+import NavBar    from './components/NavBar/NavBar';
+import LandingPage from './pages/LandingPage'; 
+
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        {/* stub routes for now */}
+
+    
+      <div className="page-wrapper">
+        <Routes>
+        <Route path="/" element={<LandingPage/>} />
         <Route path="/things-to-do" element={<h1>Things to do</h1>} />
         <Route path="/language"     element={<h1>Language</h1>} />
         <Route path="/history"      element={<h1>History</h1>} />
         <Route path="/kids"         element={<h1>Kids</h1>} />
-      </Routes>
+          
+        </Routes>
+      </div>
+
+      
+      
     </BrowserRouter>
   );
 }
