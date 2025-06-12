5f4e6ff3a4517cf472dfc51f74874285e524d46e
Waveyjoe <77323471+Waveyjoe@users.noreply.github.com>
2025-06-12 17:12:56 +1200

    Add Country model, controller and routes with full CRUD. Added one country with some data to begin implementing in frontend

---
+++ backend/models/Country.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const countrySchema = new mongoose.Schema(
+  {
+    countryName: {
+      type: String,
+      required: true,
+    },
+    slug: { type: String, unique: true, required: true },
+    intro: String,
+    history: String,
+    language: [{ phrase: String, meaning: String }],
+    exploration: {
+      activities: [
+        {
+          title: String,
+          description: String,
+          imageURL: String,
+          liked: { type: Boolean, default: false }.
+        },
+      ],
+      // food: [String],
+      // events: [String],
+    },
+    // images: [{ url: String, caption: String }],
+    flagUrl: String,
+    // moreInfo: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Country", countrySchema);
