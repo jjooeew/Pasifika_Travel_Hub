c85cfbe66b546522eb1e598df64e6c96f61e5d6c
GrahamBlackledge <gblackledge88@gmail.com>
2025-06-22 16:14:43 +1200

    cheanged firebase to read .env

--- frontend-web/src/firebase.js
+++ frontend-web/src/firebase.js
@@ -5,14 +5,15 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNbzsFuhyoFVf3kDpSto07zSZvvpwQWV4",
-  authDomain: "pasifikahub-bbacc.firebaseapp.com",
-  projectId: "pasifikahub-bbacc",
-  storageBucket: "pasifikahub-bbacc.firebasestorage.app",
-  messagingSenderId: "56624665996",
-  appId: "1:56624665996:web:5058de85d3d2c4a6c532a9",
-  measurementId: "G-W3XZHM2WD4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
